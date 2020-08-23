5c443c36f77a76e2e28b1f939c8821732396a015
elsone <sanaaloute@gmail.com>
2020-08-23 20:07:41 +0800

    Another chievement

--- campus.py
+++ campus.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request,redirect, flash
-from forms import RegisterForm, LoginForm, PostForm, UniversityForm, ScholarshipForm, CalendarForm
+from forms import RegisterForm, LoginForm, JobForm, UniversityForm, ScholarshipForm, CalendarForm
 import os, secrets
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
@@ -9,6 +9,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore, storage
 from werkzeug.utils import secure_filename
 from random import random
+import json
 
 
 
@@ -93,6 +94,32 @@ def university():
     return render_template('pages/university.html', title = "university", docs = docs)
 
 
+################## Details
+
+@app.route('/details/scholarship_details/<int:index>',methods=['GET'])
+def scholarship_details(index):
+    data = firestore.collection(u'Scholarship').get()
+    docs = []
+    for doc in data:
+        docs.append(doc.to_dict())
+    return render_template('details/scholarship_details.html', document = docs[index])
+
+@app.route('/details/university_details/<int:index>',methods=['GET'])
+def university_details(index):
+    data = firestore.collection(u'Universities').get()
+    docs = []
+    for doc in data:
+        docs.append(doc.to_dict())
+    return render_template('details/university_details.html', document = docs[index])
+
+@app.route('/details/job_details/<int:index>',methods=['GET'])
+def job_details(index):
+    data = firestore.collection(u'jobStages').get()
+    docs = []
+    for doc in data:
+        docs.append(doc.to_dict())
+    return render_template('details/job_details.html', document = docs[index])
+
 
 # make a post
 @app.route("/pages/nouveaux",methods=['GET', 'POST'])
@@ -117,7 +144,7 @@ def contact():
 
 
 
-########  Admin upload to database
+           ########  Admin upload to database ################  
 
 ########### upload new scholarship
 @app.route("/admin/makeScho",methods=['GET', 'POST'])
@@ -137,7 +164,6 @@ def makeScho():
 ########## upload new University
 @app.route("/admin/makeUni",methods=['GET', 'POST'])
 def makeUni():
-    
     form = UniversityForm()
     if form.validate_on_submit():
         print('this is a new uni')
@@ -151,34 +177,22 @@ def makeUni():
             return redirect(url_for('home'))
     return render_template('admin/makeUni.html',form = form)
 
-
 ######### Make a new post
-@app.route("/admin/makepost",methods=['GET', 'POST'])
-def makepost():
-    form = PostForm()
+@app.route("/admin/makeJob",methods=['GET', 'POST'])
+def makeJob():
+    form = JobForm()
     if form.validate_on_submit():
+        print('this is a new uni')
         flash(f'sucessfully created', 'success')
-        if form.picture.data:
-            print('file pciked')
-            picture = save_picture(form.picture.data)
+        if form.logo.data:
+            print('file picked')
+            picture = save_logo(form.logo.data)
             picture_path = os.path.join(app.root_path,'photos',picture)
-            savePost_toDatabase(form, picture, picture_path)
+            saveUni_toDatabase(form, picture, picture_path)
             os.remove(picture_path)
             return redirect(url_for('home'))
-    return render_template('admin/makepost.html', form = form)
+    return render_template('admin/makeJob.html', form = form)
 
-####### Resize and save picture
-def save_picture(form_pic):
-    random_hex = secrets.token_hex(8)
-    _,file_ext = os.path.splitext(form_pic.filename)
-    picture_name = random_hex + file_ext
-    picture_path = os.path.join(app.root_path,'photos',picture_name)
-    form_pic.save(picture_path)
-    out_size = (1000,800)
-    image = Image.open(form_pic)
-    image.thumbnail(out_size)
-    image.save(picture_path)
-    return picture_name
 
 ####### Resize and save picture
 def save_logo(form_pic):
@@ -193,22 +207,6 @@ def save_logo(form_pic):
     image.save(picture_path)
     return picture_name
 
-################# Save a post
-def savePost_toDatabase(form, picture, picture_path):
-    firebase.storage().child('PostImages').child(picture).put(picture_path)
-    ImageUrl = firebase.storage().child('PostImages').child(picture).get_url(None)
-    docRef = firestore.collection(u'Photos_videos')
-    docRef.add({
-    u'title' : u'{}'.format(request.form.get('title')),
-    u'description' : u'{}'.format(request.form.get('description')),
-    u'image' : ImageUrl,
-    u'date'  : u'{}'.format(form.post_date),
-    u'time' : u'{}'.format(form.post_time),
-    u'order': u'{}'.format(form.post_order),
-    u'owner': u'eT1yo6RHBZNEz3smyKyzwZ0fQFV2',
-    u'type' : u'photo',
-    u'webUrl': u'webUrl',
-    })
 
 ########### Save an university
 
@@ -246,8 +244,29 @@ def saveScho_toDatabase(form, logo, picture_path):
     u'webUrl' : u'{}'.format(request.form.get('web')),
     u'date'  : u'{}'.format(form.post_date),
     u'order': u'{}'.format(form.post_order),
+    })  
+  
+  
+  ################## Save Job to DB  
+def saveJob_toDatabase(form, logo, picture_path):
+    firebase.storage().child('JobStage').child(logo).put(picture_path)
+    logoUrl = firebase.storage().child('JobStage').child(logo).get_url(None)
+    docRef = firestore.collection(u'JobStage')
+    docRef.add({
+    u'name' : u'{}'.format(request.form.get('name')),
+    u'description' : u'{}'.format(request.form.get('description')),
+    u'position' : u'{}'.format(request.form.get('position')),
+    u'country' : u'{}'.format(request.form.get('country')),
+    u'deadline' : form.deadline.data.strftime('%d/%m/%Y'),
+    u'level' : u'{}'.format(request.form.get('level')),
+    u'logo' : logoUrl,
+    u'webUrl' : u'{}'.format(request.form.get('web')),
+    u'order': u'{}'.format(form.post_order),
+    })     
     
-    })    
+    
+    
+      
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -32,11 +32,17 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
        
 
-########## Upload a post
-class PostForm(FlaskForm):
-    title = StringField('title', validators = [DataRequired(), Length(min=2)])
+########## Upload a Job
+class JobForm(FlaskForm):
+    name = StringField('title', validators = [DataRequired(), Length(min=2)])
     description = TextAreaField('description', validators = [DataRequired(), Length(min=2)])
-    picture = FileField("Choose file", validators=[FileAllowed(['jpg', 'png', 'mp4'])])
+    country = StringField('Country', validators = [DataRequired(), Length(min=2),])
+    logo = FileField("Upload logo", validators=[FileAllowed(['jpg', 'png', 'mp4','jpeg'])])
+    web = StringField('Website url', validators = [DataRequired(), Length(min=2),])
+    condtions = TextAreaField('Description', validators = [DataRequired(),])
+    position = TextAreaField('Description', validators = [DataRequired(),])
+    level = TextAreaField('Description', validators = [DataRequired(),])
+    deadline = DateField('Deadline', format='%d/%m/%Y')
     submit = SubmitField('Submit')
     post_date = date
     post_time = time
@@ -63,6 +69,7 @@ class ScholarshipForm(FlaskForm):
     name = StringField('Scholarship name', validators = [DataRequired(), Length(min=2,)])
     country = StringField('Country', validators = [DataRequired(), Length(min=2),])
     description = TextAreaField('Description', validators = [DataRequired(),])
+    condtions = TextAreaField('Description', validators = [DataRequired(),])
     advantage = TextAreaField('Advantages', validators = [DataRequired(),])
     level = StringField('level', validators = [DataRequired(),])
     logo = FileField('upload logo', validators = [FileAllowed(['jpg', 'png', 'jpeg',])])
